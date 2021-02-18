7253f267f61025eca8d73cd6ee9cc452af2d8195
Rodrigo Matheus <rodrigo.matheus@live.com>
2021-02-18 20:56:31 -0300

    multivariate Cauchy distribution

--- R/multivariate_distributions.R
+++ R/multivariate_distributions.R
@@ -129,3 +129,71 @@ st <- function(){
   out
 
 }
+
+### Multivariate Cauchy --------------------------------------------------------
+# Adapted from 'LaplacesDeamon' package
+cauchy <- function(){
+  out <- list()
+
+  ### Multivariate density
+  out$Md <- function(x, P = diag(d), checkSymmetry = TRUE){
+
+    if (is.vector(x))
+      x <- matrix(x, ncol = length(x))
+
+    d <- ncol(x)
+
+    if (!missing(P)) {
+      if (d != ncol(P))
+        stop("\nx and P have non-conforming size\n")
+
+      if (checkSymmetry && !isSymmetric(P, tol = sqrt(.Machine$double.eps),
+                                        check.attributes = FALSE))
+        stop("\nP must be a symmetric matrix\n")
+
+    }
+
+    dec <- tryCatch(chol(P), error = function(e) e)
+    if (inherits(dec, "error")) {
+      x.is.mu <- colSums(t(x) != rep(0, d)) == 0
+      logretval <- rep.int(-Inf, nrow(x))
+      logretval[x.is.mu] <- Inf
+    }else {
+      tmp <- backsolve(dec, t(x), transpose = TRUE)
+      rss <- colSums(tmp^2)
+      logretval <- lgamma((1 + d)/2) - (lgamma(0.5) + (d/2) *
+                                          log(pi) + sum(log(diag(dec))) + ((1 + d)/2) * log(1 + rss))
+
+    }
+
+    names(logretval) <- rownames(x)
+    exp(logretval)
+  }
+
+  ### Random generation
+  out$Mr <- function(n, P = diag(d), checkSymmetry = TRUE){
+
+    d <- dim(P)[1]
+
+    if (checkSymmetry && !isSymmetric(P, tol = sqrt(.Machine$double.eps),
+                                      check.attributes = FALSE)) {
+      stop("\nP must be a symmetric matrix\n")
+    }
+
+    ev <- eigen(P, symmetric = TRUE)
+    if (!all(ev$values >= -sqrt(.Machine$double.eps) * abs(ev$values[1]))) {
+      warning("\nP is numerically not positive semidefinite\n")
+    }
+
+    z <- stats::rchisq(n, 1)
+    z[which(z == 0)] <- 1e-100
+    retval <- gaussian()$Mr(n, P)
+    retval <- retval/sqrt(z)
+  }
+
+  ## Name
+  out$name <- "Cauchy"
+
+  out
+
+}
