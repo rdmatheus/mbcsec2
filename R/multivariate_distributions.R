5b68de844855a1b96fef8398b12a363ae41cd577
Rodrigo Matheus <rodrigo.matheus@live.com>
2021-02-18 21:11:02 -0300

    multivariate double exponential distribution

--- R/multivariate_distributions.R
+++ R/multivariate_distributions.R
@@ -197,3 +197,70 @@ cauchy <- function(){
   out
 
 }
+
+### Multivariate double exponential --------------------------------------------
+# Adapted from 'LaplacesDeamon' package
+dexponential <- function(){
+  out <- list()
+
+  ### Multivariate density
+  out$Md <- function(x, P = diag(d), checkSymmetry = TRUE){
+
+    if (is.vector(x))
+      x <- matrix(x, ncol = length(x))
+
+    d <- ncol(x)
+
+    if (!missing(P)) {
+      if (d != ncol(P))
+        stop("\nx and P have non-conforming size\n")
+
+      if (checkSymmetry && !isSymmetric(P, tol = sqrt(.Machine$double.eps),
+                                        check.attributes = FALSE))
+        stop("\nP must be a symmetric matrix\n")
+
+    }
+
+    dec <- tryCatch(chol(P), error = function(e) e)
+    if (inherits(dec, "error")) {
+      x.is.mu <- colSums(t(x) != rep(0, d)) == 0
+      logretval <- rep.int(-Inf, nrow(x))
+      logretval[x.is.mu] <- Inf
+    }else {
+      tmp <- backsolve(dec, t(x), transpose = TRUE)
+      rss <- colSums(tmp^2)
+      logretval <- log(2) - log(2 * pi) * (d/2) + sum(log(diag(dec))) +
+        (log(pi) - log(2) + log(2 * rss) * 0.5) * 0.5 -
+        sqrt(2 * rss) - log(rss/2) * 0.5 * (d/2 - 1)
+    }
+
+    names(logretval) <- rownames(x)
+    exp(logretval)
+  }
+
+  ### Random generation
+  out$Mr <- function(n, P = diag(d), checkSymmetry = TRUE){
+
+    d <- dim(P)[1]
+
+    if (checkSymmetry && !isSymmetric(P, tol = sqrt(.Machine$double.eps),
+                                      check.attributes = FALSE)) {
+      stop("\nP must be a symmetric matrix\n")
+    }
+
+    ev <- eigen(P, symmetric = TRUE)
+    if (!all(ev$values >= -sqrt(.Machine$double.eps) * abs(ev$values[1]))) {
+      warning("\nP is numerically not positive semidefinite\n")
+    }
+
+    e <- matrix(stats::rexp(n, 1), n, d)
+    z <- gaussian()$Mr(n, P)
+    retval <- sqrt(e) * z
+  }
+
+  ## Name
+  out$name <- "Double explonential"
+
+  out
+
+}
